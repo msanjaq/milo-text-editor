61cfdc10385cf5cbfe40fef7d43e45f92e2bc9b7
msanjaq <sanjaq.m@gmail.com>
2018-01-04 11:48:39 -0800

    Screen is refreshed on text editor exit

--- milo.c
+++ milo.c
@@ -13,6 +13,9 @@ struct termios orig_termios;
 /* terminal */
 void die(const char* s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -67,6 +70,8 @@ void editorProcessKeyPress()
 	char c = editorReadKey();
 	switch (c) {
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;	
 	}

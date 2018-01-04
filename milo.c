ffcc3aad0ee551dedb2b2b7b4b4f12e2942068ca
msanjaq <sanjaq.m@gmail.com>
2018-01-04 00:01:08 -0800

    cntrl-m no longer does carriage return

--- milo.c
+++ milo.c
@@ -17,7 +17,7 @@ void enableRawMode()
         atexit(disableRawMode);
 
         struct termios raw;
-        raw.c_iflag &= ~(IXON);
+        raw.c_iflag &= ~(ICRNL|IXON);
         raw.c_lflag &= ~(ECHO|ICANON|ISIG|IEXTEN);
         tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }

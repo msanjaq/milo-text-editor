a74bed45cea85ad5eac24253c8df0c9a2f1a2e28
msanjaq <sanjaq.m@gmail.com>
2018-01-04 15:52:55 -0800

    There is now a welcome message

--- milo.c
+++ milo.c
@@ -7,6 +7,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+#define MILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
@@ -133,7 +135,18 @@ void editorDrawRows(struct abuf* ab)
 {
 	int y;
 	for (y = 0; y < E.screenrows; ++y){
-		abAppend(ab, "~", 1);
+		if (y == E.screenrows/3){
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome),
+				"Milo editor -- verson %s", MILO_VERSION);
+			if (welcomelen > E.screencols)
+				welcomelen = E.screencols;
+
+			abAppend(ab, welcome, welcomelen);
+		} else {
+			abAppend(ab, "~", 1);
+		}
+
 		abAppend(ab, "\x1b[K", 3);
 		if (y < E.screenrows - 1)
 			abAppend(ab, "\r\n", 2);
